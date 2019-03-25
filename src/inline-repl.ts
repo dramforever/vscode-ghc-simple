9c859f5d3a2a802498e111bd4477667c092f0b1a
dramforever <dramforever@live.com>
2019-03-25 10:31:04 +0800

    [repl] Fix stale commands when typing
    The code lenses will sometimes use stale document text if the user types
    too fast and the code lenses haven't updated yet. Fix this case

--- src/inline-repl.ts
+++ src/inline-repl.ts
@@ -6,14 +6,69 @@ import { haskellReplLine, getFeatures } from './utils';
 export function registerInlineRepl(ext: ExtensionState) {
     const availableRepl: WeakMap<vscode.TextDocument, [vscode.Range, vscode.Command][]> = new WeakMap();
 
+    function parseReplBlockAt(
+        document: vscode.TextDocument, lineNum: number):
+        [number, null | {
+            headerRange: vscode.Range,
+            outputRange: vscode.Range,
+            commands: string[],
+            prefix: string
+        }] {
+        const { lineCount } = document;
+        const headerLine = document.lineAt(lineNum);
+        const header = headerLine.text.replace(/\s+$/, '');
+        const headerRes = haskellReplLine.exec(header);
+        if (headerRes !== null) {
+            const headerLineNum = lineNum;
+            const prefix = headerRes[1] || '';
+            const commands = [];
+            for (; lineNum < lineCount; lineNum ++) {
+                const line = document.lineAt(lineNum).text.replace(/\s+$/, '');
+                const lineRes = haskellReplLine.exec(line);
+                if (line.startsWith(prefix) && lineRes !== null) {
+                    commands.push(lineRes[2])
+                } else {
+                    break;
+                }
+            }
+
+            const outputLineNum = lineNum;
+
+            for (; lineNum < lineCount; lineNum ++) {
+                const line = document.lineAt(lineNum).text.replace(/\s+$/, '');
+
+                if (line == prefix.replace(/\s+$/, '')) {
+                    lineNum ++;
+                    break;
+                }
+                if (haskellReplLine.test(line) || ! line.startsWith(prefix))
+                    break;
+            }
+
+            const endLineNum = lineNum;
+
+            const headerRange = new vscode.Range(
+                document.lineAt(headerLineNum).range.start,
+                document.lineAt(outputLineNum - 1).range.end);
+
+            const outputRange = new vscode.Range(
+                document.lineAt(outputLineNum).range.start,
+                outputLineNum == endLineNum
+                    ? document.lineAt(outputLineNum).range.start
+                    : document.lineAt(endLineNum - 1).range.end);
+
+            return [ lineNum, { headerRange, outputRange, commands, prefix } ];
+        } else {
+            return [ lineNum + 1, null ];
+        }
+    }
+
     async function inlineReplRun (
         textEditor: vscode.TextEditor,
         edit: vscode.TextEditorEdit,
         arg?: {
-            range: vscode.Range,
-            commands: string[],
-            prefix: string,
-            hasRan: { flag: boolean },
+            headerLineNum: number
+            isRunning: { flag: boolean },
             batch?: boolean
         }): Promise<void> {
         if (typeof arg === 'undefined') {
@@ -24,22 +79,30 @@ export function registerInlineRepl(ext: ExtensionState) {
                     break;
                 }
         } else {
-            const { range, commands, prefix, hasRan } = arg;
-            if (hasRan.flag) return;
-            hasRan.flag = true;
+            const { headerLineNum, isRunning } = arg;
+            if (isRunning.flag) return;
+            isRunning.flag = true;
+            try {
+                const [ , res ] = parseReplBlockAt(textEditor.document, headerLineNum);
+                if (res === null) return;
 
-            const session = await startSession(ext, textEditor.document);
-            await session.loading;
-            await session.ghci.sendCommand(
-                `:module *${session.getModuleName(textEditor.document.uri.fsPath)}`);
-            const response = await session.ghci.sendCommand(commands);
-            if (response[0] == '') response.shift();
-            if (response[response.length - 1] == '') response.pop();
-            const filtRsponse = response.map(s => prefix + (s == '' ? '<BLANKLINE>' : s));
-            const end = range.isEmpty ? '\n' : '';
-            const replacement = filtRsponse.map(s => s + '\n').join('') + prefix.replace(/\s+$/, '') + end;
-            await textEditor.edit(e => e.replace(range, replacement),
-                { undoStopBefore: ! arg.batch, undoStopAfter: ! arg.batch });
+                const { outputRange, commands, prefix } = res;
+
+                const session = await startSession(ext, textEditor.document);
+                await session.loading;
+                await session.ghci.sendCommand(
+                    `:module *${session.getModuleName(textEditor.document.uri.fsPath)}`);
+                const response = await session.ghci.sendCommand(commands);
+                if (response[0] == '') response.shift();
+                if (response[response.length - 1] == '') response.pop();
+                const filtRsponse = response.map(s => prefix + (s == '' ? '<BLANKLINE>' : s));
+                const end = outputRange.isEmpty ? '\n' : '';
+                const replacement = filtRsponse.map(s => s + '\n').join('') + prefix.replace(/\s+$/, '') + end;
+                await textEditor.edit(e => e.replace(outputRange, replacement),
+                    { undoStopBefore: ! arg.batch, undoStopAfter: ! arg.batch });
+            } finally {
+                isRunning.flag = false;
+            }
         }
     }
 
@@ -49,8 +112,8 @@ export function registerInlineRepl(ext: ExtensionState) {
         Promise<void> {
         if (! availableRepl.has(textEditor.document)) return;
         textEditor.edit(() => {}, { undoStopBefore: true, undoStopAfter: false });
-        for (const [ hr, cmd ] of availableRepl.get(textEditor.document))
-        await inlineReplRun(textEditor, edit, Object.assign({}, cmd.arguments[0], { batch: true }));
+        for (const [ , cmd ] of availableRepl.get(textEditor.document))
+            await inlineReplRun(textEditor, edit, Object.assign({}, cmd.arguments[0], { batch: true }));
         textEditor.edit(() => {}, { undoStopBefore: false, undoStopAfter: true });
     }
 
@@ -75,63 +138,26 @@ export function registerInlineRepl(ext: ExtensionState) {
         const available: [vscode.Range, vscode.Command][] = [];
         const lineCount = document.lineCount;
         for (let lineNum = 0; lineNum < lineCount;) {
-            const headerLineNum = lineNum;
-            const headerLine = document.lineAt(lineNum);
-            const header = headerLine.text.replace(/\s+$/, '');
-            const headerRes = haskellReplLine.exec(header);
-            if (headerRes !== null) {
-                const prefix = headerRes[1] || '';
-                const commands = [];
-                for (; lineNum < lineCount; lineNum ++) {
-                    const line = document.lineAt(lineNum).text.replace(/\s+$/, '');
-                    const lineRes = haskellReplLine.exec(line);
-                    if (line.startsWith(prefix) && lineRes !== null) {
-                        commands.push(lineRes[2])
-                    } else {
-                        break;
-                    }
-                }
-
-                const outputLineNum = lineNum;
-
-                for (; lineNum < lineCount; lineNum ++) {
-                    const line = document.lineAt(lineNum).text.replace(/\s+$/, '');
-
-                    if (line == prefix.replace(/\s+$/, '')) {
-                        lineNum ++;
-                        break;
-                    }
-                    if (haskellReplLine.test(line) || ! line.startsWith(prefix))
-                        break;
-                }
-
-                const endLineNum = lineNum;
-
-                const headerRange = new vscode.Range(
-                    document.lineAt(headerLineNum).range.start,
-                    document.lineAt(outputLineNum - 1).range.end);
-
-                const range = new vscode.Range(
-                    document.lineAt(outputLineNum).range.start,
-                    outputLineNum == endLineNum
-                        ? document.lineAt(outputLineNum).range.start
-                        : document.lineAt(endLineNum - 1).range.end);
-
+            const [ lineNum1, res ] = parseReplBlockAt(document, lineNum);
+            lineNum = lineNum1;
+            if (res !== null) {
+                const { headerRange } = res;
                 const command: vscode.Command = {
                     title: 'Run in GHCi',
                     command: 'vscode-ghc-simple.inline-repl-run',
                     arguments: [
                         {
-                            range, commands, prefix,
-                            hasRan: { flag: false }
+                            headerLineNum: headerRange.start.line,
+                            isRunning: { flag: false }
                         }
                     ]
                 };
 
                 available.push([headerRange, command]);
-                if (codeLensEnabled) codeLenses.push(new vscode.CodeLens(headerLine.range, command));
-            } else {
-                lineNum ++;
+                if (codeLensEnabled)
+                    codeLenses.push(
+                        new vscode.CodeLens(
+                            document.lineAt(headerRange.start.line).range, command));
             }
         }
         availableRepl.set(document, available);
