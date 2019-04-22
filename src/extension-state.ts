15037cbf21c64b4c6217118356829c4fc0c5f90d
Edmund Noble <edmundnoble@gmail.com>
2019-04-22 12:41:28 -0400

    Add REPL command suffix option

--- src/extension-state.ts
+++ src/extension-state.ts
@@ -18,6 +18,11 @@ function getWorkspaceType(ext: ExtensionState, folder: vscode.WorkspaceFolder): 
     return ext.workspaceTypeMap.get(folder);
 }
 
+function splitArgs(argStr: string): string[] {
+    if (argStr === "") return [];
+    else return argStr.split(" ");
+}
+
 export async function startSession(ext: ExtensionState, doc: vscode.TextDocument): Promise<Session> {
     const folder = vscode.workspace.getWorkspaceFolder(doc.uri);
     const type = folder === undefined
@@ -27,16 +32,22 @@ export async function startSession(ext: ExtensionState, doc: vscode.TextDocument
     const session = (() => {
         if (-1 !== ['stack', 'cabal', 'cabal new', 'cabal v2'].indexOf(type)) {
             // stack or cabal
+            const cmdSuffix =
+                vscode.workspace.getConfiguration('ghcSimple', folder.uri).commandSuffix;
 
             if (! ext.workspaceManagers.has(folder))
-                ext.workspaceManagers.set(folder, new Session(ext, type, 'workspace', folder.uri));
+                ext.workspaceManagers.set(folder,
+                    new Session(ext, type, splitArgs(cmdSuffix), 'workspace', folder.uri));
 
             return ext.workspaceManagers.get(folder);
         } else {
             // bare or bare-stack
+            const cmdSuffix =
+                vscode.workspace.getConfiguration('ghcSimple', doc.uri).commandSuffix;
 
             if (! ext.documentManagers.has(doc))
-                ext.documentManagers.set(doc, new Session(ext, type, 'file', doc.uri));
+                ext.documentManagers.set(doc,
+                    new Session(ext, type, splitArgs(cmdSuffix), 'file', doc.uri));
 
             return ext.documentManagers.get(doc);
         }
